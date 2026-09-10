eeadb38327fd3340c62fef2d7f548a2502cc4ae8
Mike-Flagin <mike.step.odessa@gmail.com>
2026-09-10 18:22:55 +0300

    Updated Spinner.jsx component

--- frontend/src/components/Spinner/Spinner.jsx
+++ frontend/src/components/Spinner/Spinner.jsx
@@ -1,35 +1,52 @@
 import {useState} from 'react';
 import styles from './style_spinner.module.css'
 
-export default function Spinner({val, min, max, step, onChange}) {
-    const [value, setValue] = useState(val);
+/**
+ * @param {Object} props
+ * @param {number} [props.defaultValue] - Начальное значение
+ * @param {number} [props.min] - Минимальное значение
+ * @param {number} [props.max] - Максимальное значение
+ * @param {number} [props.step=1] - Шаг изменения
+ * @param {function} [props.onChange] - Колбэк при изменении значения
+ */
+export default function Spinner({defaultValue, min, max, step = 1, onChange}) {
+    const [value, setValue] = useState(defaultValue);
 
-    function handleClickPlus() {
-        if (value < max) {
-            const newValue = value + step;
-            setValue(newValue);
-            if (onChange) onChange(newValue);
-        }
+    function handleDecrement() {
+        const newValue = value - step;
+        if (min !== undefined && newValue < min) return;
+        setValue(newValue);
+        onChange?.(newValue);
     }
 
-    function handleClickMinus() {
-        if (value > min) {
-            const newValue = value - step;
-            setValue(newValue);
-            if (onChange) onChange(newValue);
-        }
+    function handleIncrement() {
+        const newValue = value + step;
+        if (max !== undefined && newValue > max) return;
+        setValue(newValue);
+        onChange?.(newValue);
     }
+
+    const isMinReached = min !== undefined && value <= min;
+    const isMaxReached = max !== undefined && value >= max;
 
     return (
         <div className={styles.spinner}>
-            <button onClick={handleClickMinus}>
-                -
+            <button
+                type="button"
+                className={styles.button}
+                onClick={handleDecrement}
+                disabled={isMinReached}
+            >
+                −
             </button>
-            <div>
-                <span>{value}</span>
-            </div>
-            <button onClick={handleClickPlus}>
-                <span>+</span>
+            <span className={styles.value}>{value}</span>
+            <button
+                type="button"
+                className={styles.button}
+                onClick={handleIncrement}
+                disabled={isMaxReached}
+            >
+                +
             </button>
         </div>
     )
